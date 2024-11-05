a48e20ebeaa23a4e79cba5f8eb43476e2daaa91e
znzjugod <zhangnan134@huawei.com>
2024-11-05 11:47:56 +0800

    update nvme config

--- src/python/sentryPlugins/ai_block_io/config_parser.py
+++ src/python/sentryPlugins/ai_block_io/config_parser.py
@@ -74,10 +74,10 @@ class ConfigParser:
             "write_tot_lim": 50000
         },
         "latency_nvme_ssd": {
-            "read_avg_lim": 300,
-            "write_avg_lim": 300,
-            "read_tot_lim": 500,
-            "write_tot_lim": 500
+            "read_avg_lim": 10000,
+            "write_avg_lim": 10000,
+            "read_tot_lim": 50000,
+            "write_tot_lim": 50000
         },
         "latency_sata_hdd": {
             "read_avg_lim": 15000,
