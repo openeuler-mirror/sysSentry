2c0bf730d620a1f9cb6eafe4fabf2e3d706ae55e
shixuantong <shixuantong1@huawei.com>
2025-01-08 17:26:27 +0800

    enable test

--- selftest/test/test_sentryctl_type_oneshot.sh
+++ selftest/test/test_sentryctl_type_oneshot.sh
@@ -3,7 +3,7 @@
 
 source "libs/expect.sh"
 source "libs/lib.sh"
-source "test/common.sh"
+source "libs/common.sh"
 set +e
 
 tmp_log="tmp_log"
@@ -66,7 +66,7 @@ function post_test() {
 		kill -9 `pgrep -w test_task`
 		sleep 1
 	done
-   rm -rf ${tmp_log} test_task /usr/bin/test_task /etc/sysSentry/tasks/test_type_oneshot.mod
+   rm -rf ${tmp_log} test/sysSentry/test_task /usr/bin/test_task /etc/sysSentry/tasks/test_type_oneshot.mod
 }
 set -x
 run_testcase
